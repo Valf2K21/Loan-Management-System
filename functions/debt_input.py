4c1ba1a208596c92d5787389492f6d5052b0f752
Valf2K21 <officialvalfridgalinato@hotmail.com>
2023-10-11 21:47:56 +0800

    ALpha v0.3.5

--- functions/debt_input.py
+++ functions/debt_input.py
@@ -1,13 +1,48 @@
 # import dependencies
-import pyodbc
-from flask import request
+import psycopg2
+import pandas as pd
 
 # function 1: retrieve user-submitted data from debt input
-def debt_input(debt_name, debt_date, debt_amount, debt_percent, debt_interest, debt_total):
+def debt_input(tableName):
     # debug: print message to confirm that this function has been called
     print('DEBUG: FUNCTION debt_input CALLED SUCCESSFULLY!')
 
-    # test: store a print command in a variable, then return it
-    result = print('SUBMITTED:\nName: ', debt_name, '\nDate: ', debt_date, '\nAmount: ', debt_amount, '\nPercent: ', debt_percent, '\nInterest: ', debt_interest, '\nTotal: ', debt_total)
+    # store localhost, port, database, user, and password details in their respective variables
+    host = 'localhost'
+    port = '5432'
+    database = 'loan_system_db'
+    user = 'postgres'
+    password = 'valFr3d2020'
 
-    return result
+    # use psycopg2.connect() function to connect to stated PostgreSQL server
+    conn = psycopg2.connect(host = host, port = port, database = database, user = user, password = password)
+
+    # use conn.cursor() function to create a cursor for SQL command execution
+    c = conn.cursor()
+
+    # test: save passed table name to variable
+    table = tableName
+
+    # test: modifiable SQL code for table query
+    tableQuery = "SELECT * FROM " + table
+
+    # test: use created cursor to execute stated SQL code
+    c.execute(tableQuery)
+
+    # test: fetch all data gathered by the cursor
+    tableData = c.fetchall()
+
+    # test: use pd.dataframe() function to store fetched data into a dataframe
+    testDf = pd.DataFrame(tableData, columns = ['id', 'name', 'age', 'address', 'phone_number'])
+
+    # test: return dataframe
+    return testDf
+
+    # test: store a print command in a variable, then return it
+    # result = print('SUBMITTED:\nName: ', debt_name, '\nDate: ', debt_date, '\nAmount: ', debt_amount, '\nPercent: ', debt_percent, '\nInterest: ', debt_interest, '\nTotal: ', debt_total)
+
+    # return result
+
+resultDb = debt_input('tb_debtor_records')
+
+print(resultDb)
